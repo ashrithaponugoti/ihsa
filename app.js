6736cfff11e603747c46776705e02769ae7b3379
ashrithaponugoti <112123590+ashrithaponugoti@users.noreply.github.com>
2023-05-25 14:08:59 -0500

    Modified json and js files

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+// Create an Express app
+const app = express();
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
